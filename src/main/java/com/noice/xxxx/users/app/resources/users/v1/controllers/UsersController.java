7dcb0a2e0fe165b8882b3a831b3e2e830a6bc96b
Kamule <benjamin.herbomez@gmail.com>
2018-09-08 14:25:00 +0200

    Change get users return type

--- src/main/java/com/noice/xxxx/users/app/resources/users/v1/controllers/UsersController.java
+++ src/main/java/com/noice/xxxx/users/app/resources/users/v1/controllers/UsersController.java
@@ -1,15 +1,20 @@
-package com.noice.xxxx.users.app.resources.users;
+package com.noice.xxxx.users.app.resources.users.v1.controllers;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.noice.xxxx.users.app.resources.users.v1.dto.UserDto;
 
 @RestController
 public class UsersController {
 	public static final String NAME = "/users";
 	
 	@RequestMapping(value=NAME, method=RequestMethod.GET)
-    public String listeProduits() {
-        return "Un exemple d'utilisateur";
+    public List<UserDto> listeProduits() {
+        return Arrays.asList(UserDto.builder().username("Kamule").build());
     }
 }
---
+++ src/main/java/com/noice/xxxx/users/app/resources/users/v1/dto/UserDto.java
@@ -0,0 +1,10 @@
+package com.noice.xxxx.users.app.resources.users.v1.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @Builder
+public class UserDto {
+	private String username;
+}
