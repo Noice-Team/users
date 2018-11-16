200d8665eb2c62622b8a2f7335c56698e7822f46
Kamule <benjamin.herbomez@gmail.com>
2018-11-16 23:29:03 +0100

    Fix dto

--- src/main/java/com/noice/xxxx/users/app/resources/users/v1/dto/UserDto.java
+++ src/main/java/com/noice/xxxx/users/app/resources/users/v1/dto/UserDto.java
@@ -1,11 +1,20 @@
 package com.noice.xxxx.users.app.resources.users.v1.dto;
 
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter @Builder
-public class UserDto {
-	  private String _id;
+@NoArgsConstructor @AllArgsConstructor
+public class UserDto implements Serializable{
+	  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 8976385626708521557L;
+	private String _id;
 	  private String _name;
 }
